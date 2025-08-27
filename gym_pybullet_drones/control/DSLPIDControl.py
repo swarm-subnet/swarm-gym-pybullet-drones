cd6b9b9bdfe7f2c2f8efecff5bdd51909d614006
S3333F <root@vmi2678008.contaboserver.net>
2025-08-27 16:26:19 +0200

    Custom Drone

--- gym_pybullet_drones/control/DSLPIDControl.py
+++ gym_pybullet_drones/control/DSLPIDControl.py
@@ -31,8 +31,8 @@ class DSLPIDControl(BaseControl):
 
         """
         super().__init__(drone_model=drone_model, g=g)
-        if self.DRONE_MODEL != DroneModel.CF2X and self.DRONE_MODEL != DroneModel.CF2P:
-            print("[ERROR] in DSLPIDControl.__init__(), DSLPIDControl requires DroneModel.CF2X or DroneModel.CF2P")
+        if self.DRONE_MODEL not in [DroneModel.CF2X, DroneModel.CF2P, DroneModel.SWARM_TEST]:
+            print("[ERROR] in DSLPIDControl.__init__(), DSLPIDControl requires DroneModel.CF2X, DroneModel.CF2P, or DroneModel.SWARM_TEST")
             exit()
         self.P_COEFF_FOR = np.array([.4, .4, 1.25])
         self.I_COEFF_FOR = np.array([.05, .05, .05])
@@ -57,6 +57,13 @@ class DSLPIDControl(BaseControl):
                                     [+1, 0, 1],
                                     [0,  1,  -1],
                                     [-1, 0, 1]
+                                    ])
+        elif self.DRONE_MODEL == DroneModel.SWARM_TEST:
+            self.MIXER_MATRIX = np.array([
+                                    [-.5, -.5, -1],
+                                    [-.5,  .5,  1],
+                                    [.5, .5, -1],
+                                    [.5, -.5,  1]
                                     ])
         self.reset()
 
